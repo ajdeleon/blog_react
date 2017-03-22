99140da0edbd854a8bc0e07dfda06220124f9bb5
AJ de Leon <ajdeleon1064@gmail.com>
2017-03-22 00:43:36 -0400

    first commit

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
-    );
+      <div>
+        {this.props.children}
+      </div>
+    ); //in routes, nested components are passed to parent as this.props.children
   }
 }
